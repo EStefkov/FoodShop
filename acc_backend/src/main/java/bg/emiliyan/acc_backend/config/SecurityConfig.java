77ca3bf080358e6c92127432826fb6a765841b33
Emiliyan Stefkov <93265496+EStefkov@users.noreply.github.com>
2025-10-06 17:22:00 +0300

    fixing port issues

--- acc_backend/src/main/java/bg/emiliyan/acc_backend/config/SecurityConfig.java
+++ acc_backend/src/main/java/bg/emiliyan/acc_backend/config/SecurityConfig.java
@@ -57,6 +57,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         // ✅ Добавяме заглавията, които браузърът и axios очакват
         configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 //        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
 //        configuration.setExposedHeaders(List.of("Authorization"));
